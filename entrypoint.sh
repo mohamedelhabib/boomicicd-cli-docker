57cff4c079f257cd0b4d96f98ecea1888984d620
Mohamed EL Habib <elhabib_med@yahoo.fr>
2021-02-14 09:36:58 +0100

    enable debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 export baseURL=https://api.boomi.com/api/rest/v1/${accountId}
+# set -x
 
 if [ $# -eq 0 ]
   then
   echo "No arguments supplied. Launch one of the following command $(ls bin/*.sh)"
 fi
-
+export SHELLOPTS
 exec $@
