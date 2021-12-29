29c74da60661b1d52fb938ced7665ecc174417cf
Mohamed EL Habib <elhabib_med@yahoo.fr>
2021-12-29 22:02:15 +0100

    added deleteDeployedPackage : use queryPackagedComponent and queryDeployedPackage instead of the deprecated queryDeployment

--- actions/deleteDeployedPackage/deleteDeployedPackage.sh
+++ actions/deleteDeployedPackage/deleteDeployedPackage.sh
@@ -3,7 +3,7 @@
 #!/bin/bash
 source bin/common.sh
 #delete DeployedPackage by <envName> <processName>
-ARGUMENTS=(envName processName)
+ARGUMENTS=(envName processName packageVersion)
 
 inputs "$@"
 if [ -z "${envName}" ] ; then
@@ -14,24 +14,36 @@ if [ -z "${processName}" ] ; then
 	echoe "Missing processName param"
 	exit 255
 fi
+if [ -z "${packageVersion}" ] ; then
+	echoe "Missing packageVersion param"
+	exit 255
+fi
+
 
 bin/queryEnvironment.sh env="${envName}" classification='*'
-targetEnvId=$(jq -r ".result[0].id" ${WORKSPACE}/out.json)
+targetEnvId=$(jq -r ".result[0].id" ${WORKSPACE}/out.json | sed 's|null||g')
 if [ -z "${targetEnvId}" ] ; then
 	echoe "Environment not found"
 	exit 255
 fi
 
 bin/queryProcess.sh processName="${processName}"
-targetProcessId=$(jq -r ".result[0].id" ${WORKSPACE}/out.json)
+targetProcessId=$(jq -r ".result[0].id" ${WORKSPACE}/out.json | sed 's|null||g')
 if [ -z "${targetProcessId}" ] ; then
 	echoe "Process not found"
 	exit 255
 fi
 
-bin/queryDeployment.sh envId=${targetEnvId} processId=${targetProcessId} current=True
-targetPackageId=$(jq -r '.result[0].id' ${WORKSPACE}/out.json)
+bin/queryPackagedComponent.sh envId=${targetEnvId} componentType='Process' componentId=${targetProcessId} packageVersion=${packageVersion}
+targetPackageId=$(jq -r '.result[0].packageId' ${WORKSPACE}/out.json | sed 's|null||g')
 if [ -z "${targetPackageId}" ] ; then
+	echoe "Process ${processName} not deployed into ${envName}"
+	exit 0
+fi
+
+bin/queryDeployedPackage.sh envId=${targetEnvId} packageId=${targetPackageId}
+targetDeploymentId=$(jq -r '.result[0].deploymentId' ${WORKSPACE}/out.json | sed 's|null||g')
+if [ -z "${targetDeploymentId}" ] ; then
 	echoe "Process ${processName} not deployed into ${envName}"
 	exit 0
 fi
@@ -42,5 +54,5 @@ function deleteAPI {
 	curl --fail -s -X DELETE -u $authToken -H "${h1}" -H "${h2}" "$URL" >"${WORKSPACE}"/out.json
 }
 
-URL=$baseURL/DeployedPackage/${targetPackageId}
+URL=$baseURL/DeployedPackage/${targetDeploymentId}
 deleteAPI
